709744b82c8254f789c2b69585da807dd834a2a9
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-06-27 08:10:32 +0200

    finish

--- WinPlex/Views/MainPage.xaml.cs
+++ WinPlex/Views/MainPage.xaml.cs
@@ -2,16 +2,11 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Windows.UI.Xaml.Controls;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Input;
-using Windows.ApplicationModel;
 using Windows.UI.Xaml;
 using WinPlex.Helpers;
 using WinPlex.Services;
+using WinPlex.Models;
+using Windows.Devices.Geolocation;
 
 namespace WinPlex.Views
 {
@@ -37,12 +32,23 @@ namespace WinPlex.Views
 
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            Singleton<ToastNotificationsService>.Instance.ShowToastNotificationCustom("coucou");
-            Console.Write(postName.Text);
-            Console.Write(postContent.Text);
-            Console.Write(postImage.Text);
+            Geolocator geoLocator = new Geolocator();
+            GeolocationAccessStatus accessStatus = await Geolocator.RequestAccessAsync();
+            if (accessStatus == GeolocationAccessStatus.Allowed)
+            {
+                geoLocator.DesiredAccuracy = PositionAccuracy.Default;
+                Geoposition pos = await geoLocator.GetGeopositionAsync();
+                Post post = new Post(postName.Text, postImage.Text, pos.Coordinate.Point.Position.Latitude.ToString(), pos.Coordinate.Point.Position.Longitude.ToString(), postContent.Text, DateTime.Today);
+                PostService.SaveNewPost(post);
+                Singleton<ToastNotificationsService>.Instance.ShowToastNotificationCustom($"Post {postName.Text} successfully saved");
+            }
+
+            else if (accessStatus == GeolocationAccessStatus.Denied)
+            {
+                Singleton<ToastNotificationsService>.Instance.ShowToastNotificationCustom("You need to enable location in your windows");
+            }
         }
 
     }
