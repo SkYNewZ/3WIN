899cb3570758d180de21116c9ec545428cceccac
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-06-27 03:26:55 +0200

    Initial commit

---
+++ WinPlex/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinPlex.Models
+{
+    class Post
+    {
+        private DateTime dateTime;
+        private String content;
+        private String name;
+        private String img_url;
+        private int position;
+
+        public DateTime DateTime { get => dateTime; set => dateTime = value; }
+        public string Content { get => content; set => content = value; }
+        public string Name { get => name; set => name = value; }
+        public string Img_url { get => img_url; set => img_url = value; }
+        public int Position { get => position; set => position = value; }
+    }
+}
---
+++ WinPlex/Views/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml;
+using WinPlex.Helpers;
+using WinPlex.Services;
+
+namespace WinPlex.Views
+{
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Singleton<ToastNotificationsService>.Instance.ShowToastNotificationCustom("coucou");
+            Console.Write(postName.Text);
+            Console.Write(postContent.Text);
+            Console.Write(postImage.Text);
+        }
+
+    }
+}
